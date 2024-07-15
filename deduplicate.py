1c61f59b78fd017c1d1b78d73b75425c8b49ae35
Paul Walker <wpaulsg@gmail.com>
2024-07-15 07:07:01 -0400

    feat(deduplicate): add reverse deduplication...
    This catches transactions posted to the wrong account, or avoids
    duplicates in the event a transaction was recorded but with a wrong
    amount.

--- deduplicate.py
+++ deduplicate.py
@@ -22,6 +22,8 @@ def parse_args():
                         help="beancount file of existing transactions")
     ap.add_argument("incoming",
                         help="list of new transactions to reconcile (- for stdin)")
+    ap.add_argument("--account",
+                        help="report out-of-place transactions for account")
     ap.add_argument('--dupes', default=False, action=argparse.BooleanOptionalAction)
     return ap.parse_args()
 
@@ -29,6 +31,43 @@ def parse_args():
 def deduplicate(incoming, existing):
     window = datetime.timedelta(days=2)
     extract.mark_duplicate_entries(incoming, existing, window, similar.comparator())
+
+
+OOP_TAG = 'OUT_OF_PLACE'
+
+
+def out_of_place(incoming, existing, account):
+    date_start, date_end = oop_dates(incoming)
+    candidates = list(oop_candidates(date_start, date_end, existing, account))
+    deduplicate(candidates, incoming)
+    oop = []
+    for candidate in candidates:
+        if not DUPLICATE in candidate.meta:
+            oop.append(candidate._replace(tags=candidate.tags.union({OOP_TAG})))
+    return oop
+
+
+def oop_dates(incoming):
+    date_start = None
+    date_end = None
+    for entry in incoming:
+        if date_start == None or entry.date < date_start:
+            date_start = entry.date
+        if date_end == None or entry.date > date_end:
+            date_end = entry.date
+    if date_start == None or date_end == None:
+        raise ValueError('Failed to find dates in incoming entries')
+    return date_start, date_end
+
+
+def oop_candidates(date_start, date_end, existing, account):
+    for entry in existing:
+        if isinstance(entry, Transaction):
+            if entry.date >= date_start and entry.date <= date_end:
+                for posting in entry.postings:
+                    if posting.account == account:
+                        yield entry
+                        break
 
 
 def prune_dupes(entries):
@@ -44,6 +83,9 @@ if __name__ == "__main__":
     # parser handles '-' for stdin
     incoming = parser.parse_file(args.incoming)[0]
     deduplicate(incoming, existing)
+    if args.account is not None:
+        incoming.extend(out_of_place(incoming, existing, args.account))
     if args.dupes == False:
-        incoming = prune_dupes(incoming)
+        incoming = list(prune_dupes(incoming))
+    incoming.sort(key=lambda x:x.date)
     extract.print_extracted_entries([(args.incoming, incoming, None, None)], sys.stdout)
