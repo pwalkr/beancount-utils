71ef269e2f25ca99673a9c5510a0432114ee2c24
Paul Walker <wpaulsg@gmail.com>
2024-10-10 14:08:01 -0400

    feat: add deduplication+oop to chase csv

--- beancount_utils/importers/csv_chase_bank.py
+++ beancount_utils/importers/csv_chase_bank.py
@@ -2,6 +2,8 @@ from os import path
 from beangulp import mimetypes
 from beangulp.importers import csvbase
 
+from beancount_utils.deduplicate import mark_duplicate_entries, extract_out_of_place
+
 
 class Importer(csvbase.Importer):
     date = csvbase.Date('Post Date', '%m/%d/%Y')
@@ -17,3 +19,7 @@ class Importer(csvbase.Importer):
         with open(filepath) as fd:
             head = fd.read(1024)
         return head.startswith('Transaction Date,Post Date,Description,Category,Type,Amount')
+
+    def deduplicate(self, entries, existing):
+        mark_duplicate_entries(entries, existing, self.importer_account)
+        entries.extend(extract_out_of_place(existing, entries, self.importer_account))
