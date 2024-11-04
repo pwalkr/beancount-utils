71b6eb425421d2c7eb59a0f6028620e19ec303c3
Paul Walker <wpaulsg@gmail.com>
2024-11-03 22:31:19 -0500

    feat: add importer for Automated Benefit Services claims export

---
+++ beancount_utils/importers/csv_abs_claims.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+from decimal import Decimal
+from os import path
+from beancount.core import data
+from beangulp import importer, mimetypes
+from beangulp.importers import csvbase
+import csv
+import re
+
+
+class Importer(importer.Importer):
+    def __init__(self, account, currency, insurance_account=None, decorate=None):
+        self.importer_account = account
+        self.currency = currency
+        self.insurance_account = insurance_account
+        self.decorate = decorate
+
+    def identify(self, filepath):
+        if not path.basename(filepath).startswith('ExportClaims'):
+            return False
+        mimetype, encoding = mimetypes.guess_type(filepath)
+        if mimetype != 'text/csv':
+            return False
+        with open(filepath) as fd:
+            head = fd.read(1024)
+        return head.startswith('"Claim Number","Patient Name","Service Date"')
+
+    def account(self, filepath):
+        return self.importer_account
+
+    def extract(self, filepath, existing):
+        entries = []
+        rc = re.compile('[$,)]')
+        with open(filepath) as csvfile:
+            for entry in csv.DictReader(csvfile):
+                flag = '!'
+                date = datetime.strptime(entry['Service Date'], '%m/%d/%Y')
+                year = date.strftime('%Y')
+                account = self.importer_account.format(year=year)
+                payee = entry['Provider']
+                narration = entry['Patient Name']
+                amount = rc.sub('', entry['My Responsibility'])
+                amount = amount.replace('(', '-')
+                amount = round(-Decimal(amount), 2)
+                units = data.Amount(amount, self.currency)
+
+                meta = data.new_metadata(filepath, 0)
+
+                postings = [
+                        data.Posting(account, units, None, None, None, {
+                            'claim': entry['Claim Number'],
+                            'provider': payee
+                        })
+                    ]
+
+                if self.insurance_account:
+                    insamt = rc.sub('', entry["Total Charges"])
+                    insamt = insamt.replace('(', '-')
+                    insamt = round(-Decimal(insamt), 2)
+                    insamt = insamt - amount
+                    insamt = data.Amount(insamt, self.currency)
+                    postings.append(data.Posting(self.insurance_account, insamt, None, None, None, None))
+
+                txn = data.Transaction(meta, date.date(), flag,
+                                       payee, narration, frozenset(), frozenset(), postings)
+                entries.append(txn)
+        return entries
+
+    def deduplicate(self, entries, existing):
+        super().deduplicate(entries, existing)
+        # Decorate after marking duplicates so extra target postings don't interfere
+        if self.decorate:
+            self.decorate(entries)
