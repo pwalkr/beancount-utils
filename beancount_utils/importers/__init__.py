68feba8e2f3cfcd42241b0b20a59a2d2946a6ccf
Paul Walker <wpaulsg@gmail.com>
2024-12-20 23:45:56 -0500

    importers: add simplefin.py

--- beancount_utils/importers/__init__.py
+++ beancount_utils/importers/__init__.py
@@ -1,3 +1,4 @@
 # This list allows `import ...importers as ix`
-import beancount_utils.importers.united_csv_claims
 import beancount_utils.importers.ofx_brokerage
+import beancount_utils.importers.united_csv_claims
+import beancount_utils.importers.simplefin
