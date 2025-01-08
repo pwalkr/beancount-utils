4182fae4a737d4db61c9cc5088d1de1b91febb15
Paul Walker <wpaulsg@gmail.com>
2025-01-08 00:04:50 -0500

    wip: beancount_utils/importers/__init__.py

--- beancount_utils/importers/__init__.py
+++ beancount_utils/importers/__init__.py
@@ -1,5 +1,5 @@
 # This list allows `import ...importers as ix`
 import beancount_utils.importers.kraken_ledger_json
-import beancount_utils.importers.ofx_brokerage
+import beancount_utils.importers.merrill_ofx
 import beancount_utils.importers.united_csv_claims
 import beancount_utils.importers.simplefin
