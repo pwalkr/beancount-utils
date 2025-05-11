5c3e9f7ee7887e751319defff7fc20420dab33cc
Paul Walker <wpaulsg@gmail.com>
2025-05-10 21:41:37 -0400

    wip: beancount_utils/importers/kraken_ledger_js...

--- beancount_utils/importers/kraken_ledger_json.py
+++ beancount_utils/importers/kraken_ledger_json.py
@@ -23,10 +23,11 @@ assets_remap = {
 class Importer(beangulp.Importer):
     """An importer for Kraken Ledger JSON Export."""
 
-    def __init__(self, base_account, base_currency='USD', pnl_account="Income:PnL"):
+    def __init__(self, base_account, base_currency='USD', pnl_account="Income:PnL", stake_account="Income:Staking"):
         self.base_account = base_account
         self.base_currency = base_currency
         self.pnl_account = pnl_account
+        self.stake_account = stake_account
 
     def identify(self, filepath):
         if not filepath.lower().endswith(".json"):
@@ -46,15 +47,51 @@ class Importer(beangulp.Importer):
         with open(filepath) as f:
             data = json.load(f)
 
-        for time, group in group_ledgers(data['result']['ledger']).items():
-            meta = new_metadata(filepath, 0)
-            date = datetime.datetime.fromtimestamp(time)
-            narration = extract_narration(group, self.base_currency)
-            postings = extract_postings(group, self.base_account, self.base_currency, self.pnl_account)
-            entries.append(Transaction(meta, date.date(), '*', None, narration, frozenset(), frozenset(), postings))
-            entries.extend(extract_balances(group, filepath, date, self.base_account))
+        for time, gmap in group_ledgers(data['result']['ledger']).items():
+            for ledger_type, group in gmap.items():
+                meta = new_metadata(filepath, 0)
+                date = datetime.datetime.fromtimestamp(time)
+                if ledger_type == 'staking':
+                    entries.append(self._extract_staking(date.date(), meta, group))
+                else:
+                    narration = extract_narration(group, self.base_currency)
+                    postings = self._extract_postings(group)
+                    entries.append(Transaction(meta, date.date(), '*', None, narration, frozenset(), frozenset(), postings))
+                entries.extend(extract_balances(group, filepath, date, self.base_account))
 
         return entries
+
+    def get_asset_account(self, asset):
+        return self.base_account + ':' + asset
+
+    def _extract_staking(self, date, meta, group):
+        if len(group) != 1:
+            raise ValueError("Staking group should contain exactly one entry.")
+        ledger = group[0]
+        narration = "Staked " + ledger['asset']
+        account = self.get_asset_account(ledger['asset'])
+        amount = Amount(Decimal(ledger['amount']), ledger['asset'])
+        pmeta = {'source': f"bean-price -e 'USD:coinbase/{ledger['asset'].lower()}-usd' -d {date} --no-cache"}
+        postings = [
+            Posting(self.stake_account, None, None, None, None, None),
+            Posting(account, amount, None, None, None, pmeta),
+        ]
+        return Transaction(meta, date, '*', None, narration, frozenset(), frozenset(), postings)
+
+    def _extract_postings(self, group):
+        postings = []
+        if is_sale(group, self.base_currency):
+            postings.append(Posting(self.pnl_account, None, None, None, None, None))
+        for ledger in group:
+            asset_account = self.base_account + ':' + ledger['asset']
+            amount = Decimal(ledger['amount'])
+            # Ignore 0-amount KFEE entries
+            if ledger['asset'] == 'KFEE' and amount < 0.01:
+                continue
+            cost = extract_cost(ledger, group, self.base_currency)
+            amount = Amount(amount, ledger['asset'])
+            postings.append(Posting(asset_account, amount, cost, None, None, None))
+        return postings
 
     def deduplicate(self, entries, existing):
         mark_duplicate_entries(entries, existing, self.base_account)
@@ -73,14 +110,14 @@ def group_ledgers(ledger):
     for tid, entry in sanitize_assets(ledger):
         time = entry['time']
         if time not in groups:
-            groups[time] = []
-        groups[time].append(entry)
+            groups[time] = {}
+        if entry['type'] not in groups[time]:
+            groups[time][entry['type']] = []
+        groups[time][entry['type']].append(entry)
     return groups
 
 def extract_narration(group, base_currency):
     narration = None
-    if len(group) == 1:
-        return "Staked " + group[0]['asset']
     for entry in group:
         if entry['asset'] != base_currency:
             if Decimal(entry['amount']) > 0:
@@ -98,21 +135,6 @@ def extract_balances(group, filepath, date, base_account):
         account = base_account + ':' + ledger['asset']
         amount = Amount(Decimal(ledger['balance']), ledger['asset'])
         yield Balance(meta, date.date(), account, amount, None, None)
-
-def extract_postings(group, base_account, base_currency, pnl_account):
-    postings = []
-    if is_sale(group, base_currency):
-        postings.append(Posting(pnl_account, None, None, None, None, None))
-    for ledger in group:
-        asset_account = base_account + ':' + ledger['asset']
-        amount = Decimal(ledger['amount'])
-        # Ignore 0-amount KFEE entries
-        if ledger['asset'] == 'KFEE' and amount < 0.01:
-            continue
-        cost = extract_cost(ledger, group, base_currency)
-        amount = Amount(amount, ledger['asset'])
-        postings.append(Posting(asset_account, amount, cost, None, None, None))
-    return postings
 
 def extract_cost(ledger, group, base_currency):
     if ledger['asset'] == base_currency:
