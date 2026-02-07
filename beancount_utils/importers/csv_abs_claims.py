752ea87f37dece2e1f9510f0cdf9789646bb7cea
Paul Walker <wpaulsg@gmail.com>
2026-02-06 23:09:37 -0500

    abs: refactor around base+provider leaf accounts

--- beancount_utils/importers/csv_abs_claims.py
+++ beancount_utils/importers/csv_abs_claims.py
@@ -9,11 +9,12 @@ import re
 
 
 class Importer(importer.Importer):
-    def __init__(self, account, currency, insurance_account=None, decorate=None):
-        self.importer_account = account
+    def __init__(self, base_account, currency, insurance_account=None, decorate=None, provider_leaf=None):
+        self.base_account = base_account
         self.currency = currency
         self.insurance_account = insurance_account
         self.decorate = decorate
+        self.provider_leaf = provider_leaf
 
     def identify(self, filepath):
         if not path.basename(filepath).startswith('ExportClaims'):
@@ -26,17 +27,21 @@ class Importer(importer.Importer):
         return head.startswith('"Claim Number","Patient Name","Service Date"')
 
     def account(self, filepath):
-        return self.importer_account
+        return self.base_account
 
     def extract(self, filepath, existing):
         entries = []
         rc = re.compile('[$,)]')
         with open(filepath) as csvfile:
             for entry in csv.DictReader(csvfile):
-                flag = '!'
+                flag = '*'
                 date = datetime.strptime(entry['Service Date'], '%m/%d/%Y')
                 year = date.strftime('%Y')
-                account = self.importer_account.format(year=year)
+                account = self.full_account({
+                    "year": year,
+                    "provider": entry['Provider'],
+                    "patient": entry['Patient Name'],
+                })
                 payee = entry['Provider']
                 narration = entry['Patient Name']
                 amount = rc.sub('', entry['My Responsibility'])
@@ -44,14 +49,15 @@ class Importer(importer.Importer):
                 amount = round(-Decimal(amount), 2)
                 units = data.Amount(amount, self.currency)
 
-                meta = data.new_metadata(filepath, 0)
+                meta = data.new_metadata(filepath, 0, {
+                    'claim': entry['Claim Number'],
+                    'provider': payee
+                })
 
-                postings = [
-                        data.Posting(account, units, None, None, None, {
-                            'claim': entry['Claim Number'],
-                            'provider': payee
-                        })
-                    ]
+                postings = [data.Posting(account, units, None, None, None, None)]
+
+                entries.append(data.Transaction(meta, date.date(), flag,
+                               payee, narration, frozenset(), frozenset(), postings))
 
                 if self.insurance_account:
                     insamt = rc.sub('', entry["Total Charges"])
@@ -59,12 +65,19 @@ class Importer(importer.Importer):
                     insamt = round(-Decimal(insamt), 2)
                     insamt = insamt - amount
                     insamt = data.Amount(insamt, self.currency)
-                    postings.append(data.Posting(self.insurance_account, insamt, None, None, None, None))
+                    inspost = [data.Posting(self.insurance_account, insamt, None, None, None, None)]
+                    entries.append(data.Transaction(dict(meta), date.date(), flag,
+                                   payee, narration, frozenset(), frozenset(), inspost))
 
-                txn = data.Transaction(meta, date.date(), flag,
-                                       payee, narration, frozenset(), frozenset(), postings)
-                entries.append(txn)
         return entries
+
+    def full_account(self, entry):
+        if self.provider_leaf:
+            leaf = self.provider_leaf(entry)
+            account = self.base_account + ':' + leaf
+        else:
+            account = self.base_account
+        return account.format(year=entry["year"])
 
     def deduplicate(self, entries, existing):
         super().deduplicate(entries, existing)
