b8e2004455df7614c8cf9e0da4d12deaa7f0c24e
Paul Walker <wpaulsg@gmail.com>
2026-05-28 18:31:31 -0400

    add transamerica pdf importer

--- beancount_utils/importers/__init__.py
+++ beancount_utils/importers/__init__.py
@@ -6,5 +6,6 @@ import beancount_utils.importers.merrill_csv
 import beancount_utils.importers.merrill_ofx
 import beancount_utils.importers.simplefin
 import beancount_utils.importers.singlefin
+import beancount_utils.importers.transamerica_pdf
 import beancount_utils.importers.united_csv_claims
 import beancount_utils.importers.venmo_csv
